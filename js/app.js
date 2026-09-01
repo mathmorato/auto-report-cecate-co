474fa9a71df69acb26b7ffd9ba343a69bfb395df
mathmorato <matheus.h.h@hotmail.com>
2026-09-01 16:20:42 -0300

    v.1.7.2: remocao da opcao mec do menu de selecao de instituicoes do cadastro de equipe
    Versao anterior: v.1.7.1
    
    Nova versao: v.1.7.2
    
    Alteracoes realizadas: Removida a opcao MEC (Ministerio da Educacao) dos campos de selecao de instituicao no formulario de cadastro de integrantes.
    
    Validacoes realizadas: Lista de instituicoes conferida com sucesso no Wizard e no Catalogo Geral.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /**
  * AutoReport CECATE - Controlador Principal da Aplicação (SPA & Wizard 11 Etapas)
- * Versão: v.1.7.1
+ * Versão: v.1.7.2
  */
 
 window.icons = {
--- js/teamData.js
+++ js/teamData.js
@@ -1,6 +1,6 @@
 /**
  * AutoReport CECATE - Catálogo Oficial de Equipe Participante (UFG & FNDE)
- * Versão: v.1.7.1
+ * Versão: v.1.7.2
  */
 
 window.OFFICIAL_PRONOUNS = [
