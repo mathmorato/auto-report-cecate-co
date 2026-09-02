54e266c8a8c8dfa2084daf7ad127abb26c4104a0
mathmorato <morato@ufg.br>
2026-09-01 22:16:50 -0300

    v.2.0.9: alinha os botoes de acoes em linha unica sem quebra
    Versao anterior: v.2.0.8 | Nova versao: v.2.0.9
    
    Alteracoes: Aplicados estilos flex-wrap:nowrap e white-space:nowrap na coluna e no container de botoes da coluna Acoes na tabela de estruturas, garantindo alinhamento horizontal em linha unica.
    
    Validacoes: Sintaxe JS verificada com Node.js e versao v.2.0.9 sincronizada no rodape e arquivos do projeto.

--- js/courseStructureData.js
+++ js/courseStructureData.js
@@ -1,6 +1,6 @@
 /**
  * AutoReport CECATE - Modelo Padrão Oficial e Estrutura do Curso
- * Versão: v.2.0.8
+ * Versão: v.2.0.9
  */
 
 window.DEFAULT_COURSE_STRUCTURE = [
--- js/reportDocxGenerator.js
+++ js/reportDocxGenerator.js
@@ -1,6 +1,6 @@
 /**
  * AutoReport CECATE - Gerador de Relatórios Técnicos DOCX
- * Versão: v.2.0.8
+ * Versão: v.2.0.9
  */
 
 class ReportDocxGenerator {
